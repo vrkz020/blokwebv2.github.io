d6aae5eeb818f4eff729da0b11bf7d502e88ac10
vrkz020 <farouk-taibi@hotmail.com>
2020-01-28 20:47:09 +0100

    test
    123

---
+++ bestanden/script.js
@@ -0,0 +1,25 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+
+/* bron uitklapmenu: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible */
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+/* einde bron */
